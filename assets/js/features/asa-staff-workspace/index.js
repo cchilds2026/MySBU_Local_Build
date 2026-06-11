ab1a67b046595149f89e35800517c5a0e956d257
cchilds2026 <cchilds@sbu.edu>
2026-06-11 11:54:38 -0400

    Remove ASA staff system status panel

--- assets/js/features/asa-staff-workspace/index.js
+++ assets/js/features/asa-staff-workspace/index.js
@@ -1,7 +1,5 @@
 import { initAsaStaffInbox } from "./inbox.js";
-import { initAsaStaffSystemStatus } from "./system-status.js";
 
 export function initAsaStaffWorkspace() {
-  initAsaStaffSystemStatus();
   initAsaStaffInbox();
 }
